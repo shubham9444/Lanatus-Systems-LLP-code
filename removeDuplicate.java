e4b505e163ca2f292431cc907d0136a1c0e66d45
shubham <shubhamvaghela944@gmail.com>
2024-01-05 12:03:53 +0530

    secendt commit

--- removeDuplicate.java
+++ removeDuplicate.java
@@ -13,11 +13,15 @@ public class removeDuplicate {
 
         int j = 0;
 
-        for (int i = 0; i < n - 1; i++)
-            if (arr[i] != arr[i + 1])
-                arr[j++] = arr[i];
+        for (int i = 0; i < n - 1; i++) {
 
-        arr[j++] = arr[n - 1];
+            if (arr[i] != arr[i + 1]) {
+
+                arr[j++] = arr[i];
+            }
+
+            arr[j++] = arr[n - 1];
+        }
 
         return j;
     }
